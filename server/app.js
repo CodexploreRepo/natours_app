ac8e2c303ee7d82cbbcc69e946c3b8f9630be602
CodeXploreRePo <codexplore.channel@gmail.com>
2020-10-13 23:18:26 +0800

    Create simple API-GET in app.js

--- server/app.js
+++ server/app.js
@@ -1,9 +1,25 @@
 const express = require("express");
+const fs = require("fs");
 
 app = express();
+//JSON parse is to converted JSON file to JavaScript Object
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`),
+);
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from server side!", app: "Natours" });
+//Good Practice to Specify on API version on URL: /api/v1/
+app.get("/api/v1/tours", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    //Give the users more understanding of API response
+    results: tours.length,
+    data: {
+      tours, //equivalent to tours: tours,
+    },
+  });
+});
+
+app.post("api/v1/tours", (req, res) => {
 });
 
 const port = 3000;
